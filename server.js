185ce91cb110872b708236f3869c600c08d23746
YASHVARDHAN SINGH PATHANIA <118873075+Yash6190@users.noreply.github.com>
2025-12-13 15:20:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 9000;
 app.use(cookieParser());
 // Middlewares
 app.use(cors({
-  origin: "https://projfrontend-gaud.onrender.com",   // your React origin
+  origin: "https://projfrontend-98eb.onrender.com",   // your React origin
   credentials: true,                 // allow sending cookies
 }));
 
